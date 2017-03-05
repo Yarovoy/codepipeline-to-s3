a9ba147eec1d2fdb6259635b230618678bb88b02
Yuriy Yarovoy <yy@ohmycloud.io>
2017-03-05 22:57:07 +0300

    Implement method downloadArtifact().

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const fs = require('fs');
+
 const codePipeline = new AWS.CodePipeline();
 
 const artifactPath = '/tmp/artifact.zip';
@@ -8,10 +10,6 @@ const artifactPath = '/tmp/artifact.zip';
 exports.handler = (event, context, callback) => {
     const jobData = event['CodePipeline.job'].data;
     const jobId = event['CodePipeline.job'].id;
-
-    const accessKeyId = jobData.artifactCredentials.accessKeyId;
-    const secretAccessKey = jobData.artifactCredentials.secretAccessKey;
-    const sessionToken = jobData.artifactCredentials.sessionToken;
 
     function notifyJobSuccess() {
         return codePipeline.putJobSuccessResult({jobId}).promise()
@@ -32,7 +30,39 @@ exports.handler = (event, context, callback) => {
             .then(() => callback(error.message));
     }
 
-    downloadArtifactFromS3()
+    function downloadArtifact() {
+        return new Promise((resolve, reject) => {
+
+            console.log('Downloading CodePipeline artifact.');
+
+            const artifactData = jobData.inputArtifacts[0];
+
+            const accessKeyId = jobData.artifactCredentials.accessKeyId;
+            const secretAccessKey = jobData.artifactCredentials.secretAccessKey;
+            const sessionToken = jobData.artifactCredentials.sessionToken;
+
+            const s3 = new AWS.S3({
+                accessKeyId: accessKeyId,
+                secretAccessKey: secretAccessKey,
+                sessionToken: sessionToken,
+                params: {Bucket: artifactData.location.s3Location.bucketName},
+                signatureVersion: 'v4',
+            });
+
+            const request = s3.getObject({Key: artifactData.location.s3Location.objectKey});
+            request.on('error', reject);
+
+            const writeStream = fs.createWriteStream(artifactPath);
+            writeStream.on('error', reject);
+            writeStream.once('finish', resolve);
+
+            const readStream = request.createReadStream();
+            readStream.on('error', reject);
+            readStream.pipe(writeStream);
+        });
+    }
+
+    downloadArtifact()
         .then(notifyJobSuccess)
         .catch(notifyJobFailure);
 };
