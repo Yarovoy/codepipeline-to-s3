02475c8112abf48d8512061b5edcaa1737d526da
Yuriy Yarovoy <yy@ohmycloud.io>
2017-03-05 19:36:38 +0300

    Initialize AWS SDK and CodePipeline client.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const codePipeline = new AWS.CodePipeline();
+
 exports.handler = (event, context, callback) => {
     // TODO implement
     callback(null, 'Hello from Lambda');
