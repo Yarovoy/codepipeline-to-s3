d444e09ccc6a7a8e2213480b3082fbaa7b9ccbaf
Yuriy Yarovoy <yy@ohmycloud.io>
2017-03-05 19:40:32 +0300

    Add notifyJobSuccess() and notifyJobFailure() mathods.

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ exports.handler = (event, context, callback) => {
     const jobData = event['CodePipeline.job'].data;
     const jobId = event['CodePipeline.job'].id;
 
+    function notifyJobSuccess() {
+        return codePipeline.putJobSuccessResult({jobId}).promise()
+            .then(() => callback(null));
+    }
+
+    function notifyJobFailure(error) {
+        const params = {
+            jobId: jobId,
+            failureDetails: {
+                message: JSON.stringify(error.message),
+                type: 'JobFailed',
+                externalExecutionId: context.invokeid,
+            },
+        };
+
+        return codePipeline.putJobFailureResult(params).promise()
+            .then(() => callback(error.message));
+    }
+
